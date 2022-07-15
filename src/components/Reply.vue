3a028067b60d1ab5b62ca9b49c8f8c4e3b8e57e9
towelong <towelong@qq.com>
2022-07-15 15:51:05 +0800

    chore: update

--- src/components/Reply.vue
+++ src/components/Reply.vue
@@ -6,9 +6,11 @@ const [open, toggle] = useToggle(false)
 <template>
   <div flex flex-col pt-1 mb-1 border-b-1 border-light-700>
     <div flex>
-      <img class="w-10 h-10"
+      <img
+        class="w-10 h-10"
         src="https://gss0.bdstatic.com/6LZ1dD3d1sgCo2Kml5_Y_D3/sys/portrait/item/tb.1.683a949b.ujmqt3E6neU9jVwXXYl8yA"
-        alt="">
+        alt=""
+      >
       <p text-emerald-700 ml-1>
         我舞影凌乱
       </p>
@@ -18,18 +20,24 @@ const [open, toggle] = useToggle(false)
       <p>试过了五百多分开通不了</p>
     </div>
     <div flex justify-end text-slate-400>
-      <p mr-1>发表于 2022-06-21 12:48:08</p>
+      <p mr-1>
+        发表于 2022-06-21 12:48:08
+      </p>
       <!-- <p ml-2 text-emerald-700 cursor-pointer @click="toggle()">
         回复
       </p> -->
       <n-button text @click="toggle()">
-        <p text-emerald-700>回复</p>
+        <p text-emerald-700>
+          回复
+        </p>
       </n-button>
     </div>
     <template v-if="open">
       <n-input v-model:value="value" type="textarea" placeholder="回复" />
       <div flex justify-end my-1>
-        <n-button type="primary">发表</n-button>
+        <n-button type="primary">
+          发表
+        </n-button>
       </div>
     </template>
   </div>
