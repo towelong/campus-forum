b4078b41f019cc43a3da9876a01054af7f13566e
towelong <towelong@qq.com>
2022-08-12 10:55:59 +0800

    chore: build

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,24 @@ import Unocss from 'unocss/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules'))
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
   plugins: [
+    // splitVendorChunkPlugin(),
     Vue({
       reactivityTransform: true,
     }),
